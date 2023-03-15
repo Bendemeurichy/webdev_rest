eda64c5f32a697650b654824e8ecc51dbdc0dced
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-15 22:35:46 +0100

    added padding

--- routes/bedrijven.js
+++ routes/bedrijven.js
@@ -2,4 +2,10 @@ var express = require('express');
 var router = express.Router();
 const Bedrijf = require('../public/javascripts/mongomodels/bedrijf');
 
-/* GET b */
+/* GET bedrijven overview */
+router.get('/', async function(req, res) {
+    try {
+        const dbBedrijven = await Bedrijf.find();
+        res.render('bedrijven', {bedrijven: dbBedrijven});
+    } cat
+});
