788ca3ce937815ba3fa98d96895c16f8fb5beccd
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-15 21:42:56 +0100

    changed html

--- routes/vacatures.js
+++ routes/vacatures.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const Vacature = require('../public/javascripts/mongomodels/vacatures');
-
+const Recruiter = require('../public/javascripts/mongomodels/recruiter');
+const Bedrijf = require('../public/javascripts/mongomodels/bedrijf');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -15,5 +16,31 @@ router.get('/', async function(req, res, next) {
 });
 
 
+
+// Define a route for displaying the form
+app.get('/vacatures/new', async (req, res) => {
+    const recruiters = await Recruiter.find();
+    const bedrijven = await Bedrijf.find();
+    res.render('vacatureForm', { recruiters, bedrijven });
+});
+
+// Define a route for processing the form submission
+app.post('/vacatures', async (req, res) => {
+    const { recruiter, bedrijf, beschrijving, eisen, salarisstart, salariseind, gepubliceerd, deadline } = req.body;
+
+    const vacature = new Vacature({
+        recruiter: await Recruiter.findOne({ _id: recruiter }),
+        bedrijf: await Bedrijf.findOne({ _id: bedrijf }),
+        beschrijving,
+        eisen,
+        salaris: { start: salarisstart, end: salariseind },
+        publicatiedatum: gepubliceerd,
+        deadline
+    });
+
+    await vacature.save();
+    console.log(`New vacature by bedrijf ${bedrijf} has been added`);
+    res.redirect('/vacatures');
+});
 
 module.exports = router;
