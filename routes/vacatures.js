2a77e3b7a7ab2aa0793b0669709ef80035755f2e
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-15 20:58:38 +0100

    added vacatureadd screen

--- routes/vacatures.js
+++ routes/vacatures.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const Vacature = require('../public/javascripts/mongomodels/vacatures');
+const Recruiter = require('../public/javascripts/mongomodels/recruiter');
+const Bedrijf = require('../public/javascripts/mongomodels/bedrijf');
+const vacaturehandler= require('/public/javascripts/dbConnection/vacatureDbAccessor')
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -12,5 +15,24 @@ router.get('/', async function(req, res, next) {
         res.status(500).send('Server error');
     }
 });
+
+app.get('/new', async (req, res) => {
+    const recruiters = await Recruiter.find();
+    const bedrijven = await Bedrijf.find();
+    res.render('vacatureForm', { recruiters, bedrijven });
+});
+
+app.post('/vacatures', async (req, res) => {
+    const { recruiter, bedrijf, beschrijving, eisen, salarisstart, salariseind, gepubliceerd, deadline } = req.body;
+    try {
+        vacaturehandler.addVacature(recruiter, bedrijf, beschrijving, eisen, salarisstart, salariseind, gepubliceerd, deadline);
+    } catch (err){
+        console.error("something went wrong while saving vacature,try again")
+        alert("something went wrong while creating the vacature,try again")
+        res.redirect('/new')
+    }
+    res.redirect('/');
+});
+
 
 module.exports = router;
