7857568f6b2c0b64df8220873bcf8eaafb1624fc
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-15 22:36:48 +0100

    added padding and style

--- routes/bedrijven.js
+++ routes/bedrijven.js
@@ -7,5 +7,10 @@ router.get('/', async function(req, res) {
     try {
         const dbBedrijven = await Bedrijf.find();
         res.render('bedrijven', {bedrijven: dbBedrijven});
-    } cat
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
 });
+
+module.exports = router;
