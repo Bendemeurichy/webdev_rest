92b040de644a7750332d40655de0722e27667327
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-16 18:36:30 +0100

    fixed routing

--- routes/vacatures.js
+++ routes/vacatures.js
@@ -65,7 +65,7 @@ router.post('/new',
 
         try {
             const newVacature = await addVacature(recruiter, bedrijf, beschrijving, eisen.split(','), salarisstart, salariseind, gepubliceerd, deadline);
-            res.redirect('/vacatures')
+            res.redirect('/')
             return res.status(200).json({ message: 'New vacature added successfully!', data: newVacature });
         } catch (error) {
             console.log(error);
