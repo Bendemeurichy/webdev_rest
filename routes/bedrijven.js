59f35b97f99cdf005f7f961d86b81e6dff7ccaeb
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-16 18:56:50 +0100

    fixed routing

--- routes/bedrijven.js
+++ routes/bedrijven.js
@@ -20,14 +20,15 @@ router.get('/add', async (req, res) => {
 });
 
 router.post('/add',
-    body('naam').trim().isLength({min:1}).withMessage('Naam is verplicht').escape(),
+    /*body('naam').trim().isLength({min:1}).withMessage('Naam is verplicht').escape(),
     body('industrie').trim().isLength({min:1}).withMessage('Industrie is verplicht').isAlpha().withMessage('Enkel letters toegelaten').escape(),
     body('beschrijving').trim().isLength({min:1}).withMessage('Beschrijving is verplicht').isAlpha().withMessage('Enkel letters en cijfers toegelaten').escape()
-    , async (req, res) => {
+    ,*/
+    async (req, res) => {
     const errors = validationResult(req);
     if (! errors.isEmpty()) {
         const errorMessages = errors.array().map(error => error.msg);
-        return res.status(422).json({ errors: errorMessages });
+        return res.render('addBedrijf',{errorMessages})
     } else {
         const {naam, industrie, beschrijving} = req.body;
         try {
