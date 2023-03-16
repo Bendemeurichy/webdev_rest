2915ae848557aca101958920a4a52f296c23352b
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-16 21:10:48 +0100

    fixed routing

--- routes/bedrijven.js
+++ routes/bedrijven.js
@@ -8,7 +8,7 @@ const {body, validationResult} = require("express-validator");
 router.get('/', async function(req, res) {
     try {
         const dbBedrijven = await Bedrijf.find();
-        res.render('bedrijven', {bedrijven: dbBedrijven});
+        res.render('bedrijven', {bedrijven: dbBedrijven,errors:[]});
     } catch (err) {
         console.error(err);
         res.status(500).send('Server error');
@@ -25,11 +25,10 @@ router.post('/add',
     body('beschrijving').trim().isLength({min:1}).withMessage('Beschrijving is verplicht').isAlpha().withMessage('Enkel letters en cijfers toegelaten').escape()
     ,*/
         async (req, res) => {
-        console.log(req)
     const errors = validationResult(req);
     if (! errors.isEmpty()) {
         const errorMessages = errors.array().map(error => error.msg);
-        return res.render('addBedrijf',{errorMessages})
+        return res.render('addBedrijf',{errors:errorMessages})
     } else {
 
         const naam = req.body.naam;
