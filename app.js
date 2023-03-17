888f85b6aff16e752c56727ea1dc0a849e69da44
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-17 19:42:45 +0100

    fixing script

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ app.set('view engine', 'jade');
 
 mongoose.set('strictQuery',false);
 
-
-app.use(logger('dev'));
 app.use(express.json());
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
