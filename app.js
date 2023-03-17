5e87fef4cdf052ca0d9ab2bd0d6564d225aa193f
Ben De Meurichy <ben.demeurichy@gmail.com>
2023-03-17 15:00:43 +0100

    added werkzoekenden and recruiters

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 app.use('/vacatures',vacatureRouter);
 app.use('/bedrijven',bedrijfRouter);
-app.use('/recruiters',recruiterRouter);
+//app.use('/recruiters',recruiterRouter);
 app.use('/werkzoekende',werkzoekendeRouter);
 app.use('/', indexRouter);
 
